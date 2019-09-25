ae421d923032e9a378f0e398a726edb0e470890a
Jack Fennell <C00220386@itcarlow.ie>
2019-09-25 11:18:49 +0100

    Factorial Filter

---
+++ Assets/Scripts/FactorialFilter.cs
@@ -0,0 +1,26 @@
+﻿
+public class FactorialFilter
+{
+    public static int factorial(int x)
+    {
+        if (x == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * factorial(x - 1);
+        }
+            
+    }
+    public static int[] factorials(int[]xs)
+    {
+        int[] result = new int[xs.Length];
+        for(int i =0; i<xs.Length;i++)
+        {
+            result[i] = factorial(xs[i]);
+        }
+        return result; ;
+    }
+
+}
---
+++ Assets/Tests/CubeFilterTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            //Assert.IsTrue(false);
+
+            int[] input = { 1, 3, 5, 7, 9 };
+            int[] output = CubeFilter.cubes(input);
+            int[] expected = { 1, 27, 125, 343, 729 };
+
+            //foreach (var o in output)
+            //{
+            //    Debug.Log(o);
+            //}
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
